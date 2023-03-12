b90991786dd8a0ebf62d5d4b519d88c37e902bd1
panutat-p <pp.panutat@gmail.com>
2023-03-12 22:11:33 +0700

    feat: show JSON of GetRooms thunk in MeetingRooms

--- src/pages/dashboard/meeting-rooms.tsx
+++ src/pages/dashboard/meeting-rooms.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { Divider, Heading } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { selectAuthState, updateProfileAction } from '../../redux/auth-slice';
+import { getRoomsThunk, selectRoomsState } from '../../redux/rooms-slice';
 
 function MeetingRooms() {
-  const { profile, email } = useAppSelector(selectAuthState);
   const dispatch = useAppDispatch();
+  const { profile, email } = useAppSelector(selectAuthState);
+  const { rooms } = useAppSelector(selectRoomsState);
+  console.log('🟨 rooms.events:', rooms.events);
+
+  useEffect(() => {
+    dispatch(getRoomsThunk());
+  }, []);
 
   function reserveRoom(): void {
     dispatch(updateProfileAction());
@@ -16,13 +23,15 @@ function MeetingRooms() {
   return (
     <>
       <Heading>Meeting Room</Heading>
-      <br />
+      <Divider orientation="horizontal" />
       <p>{profile}</p>
       <p>{email}</p>
       <br />
       <Button colorScheme="teal" size="md" onClick={reserveRoom}>
         Transfer to 🐔
       </Button>
+      <Divider orientation="horizontal" />
+      <p>{JSON.stringify(rooms?.events)}</p>
     </>
   );
 }
